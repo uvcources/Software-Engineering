9acd0705fba8aa23b4ff1db71e8c5304bbc97aa2
yuvaraj <yuvaraj@192.168.0.2>
2018-08-23 22:22:27 -0500

    Array Rotation

---
+++ GoogleInterview/src/practice/array/ArrayRotation.java
@@ -0,0 +1,28 @@
+package practice.array;
+
+public class ArrayRotation {
+	void printList(int arr[][]) {
+		
+		for (int row = 0; row < arr.length; row++) {
+			for (int col = 0; col < arr.length; col++) {
+				//arr[row][col] = arr[3 - col - 1][row];
+				System.out.println(arr[col][row]);
+			}
+		} 
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7,8, 9} };
+		for (int row = 0; row < arr.length; row++) {
+			for (int col = 0; col < arr.length; col++) {
+				arr[row][col] = arr[3 - col - 1][row];
+				//System.out.println(arr[col][row]);
+			}
+		}
+		
+		ArrayRotation rr=new ArrayRotation();
+		rr.printList(arr);
+	} 
+	
+}
