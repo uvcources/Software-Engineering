f2e276e4c96633da1a923c93666d0229d3d2377d
yuvaraj <yuvaraj@192.168.0.2>
2018-08-25 12:00:09 -0500

    Rotate Image using Array

--- GoogleInterview/src/practice/array/ArrayRotation.java
+++ GoogleInterview/src/practice/array/ArrayRotation.java
@@ -6,20 +6,26 @@ public class ArrayRotation {
 		for (int row = 0; row < arr.length; row++) {
 			for (int col = 0; col < arr.length; col++) {
 				//arr[row][col] = arr[3 - col - 1][row];
-				System.out.println(arr[col][row]);
+				System.out.println(arr[row][col]);
 			}
 		} 
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7,8, 9} };
+		int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7,8, 9} }; 
+		int rotated[][]=new int[arr.length][arr.length];
+		ArrayRotation rr=new ArrayRotation();
+		System.out.println("Before Rotation");
+		rr.printList(arr);
+	
 		for (int row = 0; row < arr.length; row++) {
 			for (int col = 0; col < arr.length; col++) {
-				arr[row][col] = arr[3 - col - 1][row];
+				rotated[row][col] = arr[(arr.length-1)-col][row];
 			}
 		}
-		ArrayRotation rr=new ArrayRotation();
-		rr.printList(arr);
+		System.out.println("After Rotation");
+		rr.printList(rotated);
+		//System.out.println(arr.length);
 	} 
 	
 }
