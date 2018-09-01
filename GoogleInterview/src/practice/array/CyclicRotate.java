b471f1977c46a9b7d6ba72901f7b347aba7ae5f6
yuvaraj <yuvaraj@192.168.0.2>
2018-09-01 10:38:46 -0500

    cyclic Rotation

---
+++ GoogleInterview/src/practice/array/CyclicRotate.java
@@ -0,0 +1,36 @@
+package practice.array;
+
+public class CyclicRotate {
+ 
+	
+	public void cycleRotate(int cycle[]) {
+		
+		
+		int temp=0,i=0,n=0; 
+		  n=cycle.length-1;
+		  temp=cycle[n]; 
+		//  System.out.println(temp);
+		//  System.out.println(n);
+		  
+		  for(i=n;i>0;i--) {
+			  cycle[i]=cycle[i-1]; 
+		  }
+		cycle[i]=temp;
+	}
+	public void display(int array[]) {
+		System.out.println(array.length);
+		for(int i=0;i<=array.length-1;i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int cycle[]= {10,20,30,40,50};
+    CyclicRotate cc=new CyclicRotate();
+    cc.cycleRotate(cycle); 
+    cc.display(cycle);
+	}
+
+}
