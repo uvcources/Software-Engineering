76aef84a6f967fa20d544665937f330048caa898
yuvaraj <yuvaraj@192.168.0.2>
2018-09-02 15:45:39 -0500

    searching element from rotated array

---
+++ GoogleInterview/src/practice/array/SearchKeyRotatedArray.java
@@ -0,0 +1,43 @@
+package practice.array;
+
+public class SearchKeyRotatedArray {
+
+	public int searchElement(int array[], int low, int high, int key) {
+
+		if (low > high) {
+			return -1;
+		}
+
+		int pivot = (low + high) / 2;
+		if (array[pivot] == key) {
+			return pivot;
+		}
+
+		if (array[low] <= array[pivot]) {
+
+			if (array[low] <= key && key <= array[pivot]) {
+				return searchElement(array, low, pivot - 1, key);
+			}
+			return searchElement(array, pivot + 1, high, key);
+		}
+
+		if (key >= array[pivot] && key <= array[high]) {
+			return searchElement(array, pivot + 1, high, key);
+		}
+		return searchElement(array, low, pivot - 1, key);
+	}
+
+	public void search(int array[], int low, int high) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = { 30, 40, 50, 60, 70, 80, 10, 20 };
+		SearchKeyRotatedArray search = new SearchKeyRotatedArray();
+		int index = search.searchElement(a, 0, a.length - 1, 30);
+		System.out.println("Searched Element at " + index);
+
+	}
+
+}
